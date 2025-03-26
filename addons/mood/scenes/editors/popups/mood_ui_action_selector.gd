cba9747b6b39ccd9f13122b1eadf5e28433f5441
Zoey Lome <zoey.lome@avant.com>
2025-03-26 10:33:20 -0500

    fix: fix missing custom actions. (#5)
    * chore: fix missing custom actions.
    
    * chore: sorting.
    
    * feat: 'custom' actions, sorting, and ensuring selected items stay present.
    
    * fix: don't duplicate-add.

--- addons/mood/scenes/editors/popups/mood_ui_action_selector.gd
+++ addons/mood/scenes/editors/popups/mood_ui_action_selector.gd
@@ -3,6 +3,7 @@ extends ConfirmationDialog
 
 @export var item_list: ItemList
 @export var action_search: LineEdit
+@export var custom_action_input: LineEdit
 @export var selected_items: Array[StringName]
 
 signal on_actions_selected(actions: Array[StringName])
@@ -26,10 +27,13 @@ func _ready() -> void:
 
 func _refresh_items() -> void:
 	item_list.clear()
+	InputMap.load_from_project_settings() # reload to pick custom actions.
 	
 	var filter: String = ""
 	if is_instance_valid(action_search):
 		filter = action_search.text
+
+	var needs_adding := selected_items.duplicate()
 
 	for action: StringName in InputMap.get_actions():
 		if len(filter) > 0 and not action.begins_with(filter):
@@ -39,6 +43,14 @@ func _refresh_items() -> void:
 
 		if String(action) in selected_items:
 			item_list.select(idx, false)
+			needs_adding.erase(action)
+
+	# always add selected actions at the end if they were not added.
+	for remainder: StringName in needs_adding:
+		var idx := item_list.add_item(String(remainder))
+		item_list.select(idx, false)
+	
+	item_list.sort_items_by_text()
 
 func _on_canceled() -> void:
 	pass # Replace with function body.
@@ -59,3 +71,12 @@ func _on_item_list_multi_selected(index: int, selected: bool) -> void:
 		selected_items.erase(sn_action)
 	elif sn_action not in selected_items and selected:
 		selected_items.append(sn_action)
+
+
+func _on_add_action_button_pressed() -> void:
+	if is_instance_valid(custom_action_input):
+		var sn_action := StringName(custom_action_input.text)
+		if sn_action not in selected_items:
+			selected_items.append(sn_action)
+			_refresh_items()
+			custom_action_input.text = ""
