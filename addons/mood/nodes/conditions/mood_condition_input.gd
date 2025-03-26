a2b23f85b0944abb06161e212b67dc4367aa72f3
Zoey Lome <zoey.lome@avant.com>
2025-03-26 13:48:56 -0500

    feat: extend and improve MoodConditionInput.

--- addons/mood/nodes/conditions/mood_condition_input.gd
+++ addons/mood/nodes/conditions/mood_condition_input.gd
@@ -97,31 +97,60 @@ const SubEditor := preload("res://addons/mood/scenes/editors/mood_ui_condition_i
 ## are pressed, as per all the below rules.
 @export var invert_validity := false
 
-@export_group("Condition Rules", "rule")
-## If true, this condition is valid only when ALL actions are pressed/echoed, instead
-## of ANY actions in the list.
-@export var rule_all_actions: bool = true
-## This condition becomes valid whenever the initial press occurs. It may become
-## invalid in several ways:
-## * InvalidTrigger.MOOD_CHANGE -- the rule is valid only until the mood changes.
-## * InvalidTrigger.TIMEOUT -- the rule is valid until a timeout occurs.
-## * InvalidTrigger.RELEASE -- the rule becomes invalid as soon as the action
-##   (or actions if rule_all_actions is true) is released.
-## * InvalidTrigger.TIMEOUT_OR_RELEASE -- TIMEOUT + RELEASE.
-@export var rule_become_invalid_when := InvalidTrigger.RELEASE
-## If [param rule_become_invalid_when] is InvalidTrigger.TIMEOUT, how many seconds
-## until the rule becomes invalid again
-@export_range(0.0, 20.0, 0.25, "or_greater") var rule_timeout_sec := 0.25
-## If true and [param rule_become_invalid_when] is InvalidTrigger.TIMEOUT, echoes
-## of the action(s) will refresh the timeout.
-@export var rule_timeout_refresh_on_echo := false
-## If true and [param rule_becomes_invalid_when] is InvalidTrigger.RELEASE or TIMEOUT_OR_RELEASE,
-## the validity will only be cleared once all actions are released, instead of
-## when any action is released.
-@export var rule_only_invalid_when_all_released := false
-## If true, the timeout timer will be cleared when any/all actions are released,
-## as per rule_only_invalid_when_all_released
-@export var rule_reset_timer_on_release := true
+@export_group("Trigger Conditions", "trigger_")
+
+## By default, inputs are processed and handled generically and globally,
+## meaning that if you press [code]ui_accept[/code] then [b]all[/b]
+## [code]MoodConditionInput[/code] nodes watching that action will flag it as
+## being pressed. If this is set to [code]true[/code], though, it will only
+## process the input if the parent [member MoodChild.mood] is the
+## [member MoodMachine.current_mood].
+@export var trigger_consider_only_if_current := false:
+	set(val):
+		trigger_consider_only_if_current = val
+		property_list_changed.emit() # allow_echo_to_trigger hide/show
+
+## Generally, an action will be flagged as pressed when the initial press occurs.
+## However, if [member trigger_consider_only_if_current] is set, then a pressed button
+## may [i]not[/i] trigger that tracking. This flag allows echo presses to trigger
+## the pressed flag in that case.
+@export var trigger_echo_counts_as_press := false
+
+## If true, this condition is valid only when ALL actions are pressed/echoed,
+## instead of ANY actions in the list.
+@export var trigger_require_all_actions := false
+
+@export_group("Rule Clearance", "clear_")
+
+## If true, reset validity when entering the mood this condition belongs to.
+@export var clear_on_mood_enter := false
+## If true, reset validity when exiting the mood this condition belongs to.
+@export var clear_on_mood_exit := false
+## If true, reset validity when one or all actions are released. see
+## [member clear_on_release_only_when_all_are_released] for more information.
+@export var clear_on_action_release := true:
+	set(val):
+		clear_on_action_release = val
+		property_list_changed.emit()
+## If true, reset validity when timeout configuration applies.
+@export var clear_on_timeout := false:
+	set(val):
+		clear_on_timeout = val
+		property_list_changed.emit()
+## If true, require no actions to be pressed to trigger action-release clearing.
+## Otherwise, the release of any action will clear validity, [i]even if other
+## actions are still pressed[/i].
+@export var clear_on_release_only_when_all_are_released := true
+
+@export_group("Timeout Clearance", "timeout_")
+## If [member clear_on_timeout] is [code]true[/code], how many seconds
+## until the rule clears.
+@export_range(0.0, 20.0, 0.25, "or_greater") var timeout_timeout_seconds := 0.25
+## If true, an action being echoed will reset the timeout timer.
+@export var timeout_reset_on_action_echo := false
+## If true, the timeout timer will be zeroed when an action is released, as per
+## the [member clear_on_release_only_when_all_are_released] flag logic.
+@export var timeout_clear_on_action_release := true
 
 #endregion
 
@@ -141,8 +170,7 @@ var _timer: SceneTreeTimer
 
 ## Connect to the [InputTracker] action signals when ready.
 func _ready() -> void:
-	if invert_validity:
-		_valid = true
+	_valid = invert_validity
 
 	if has_node(^"/root/InputTracker"): # singleton
 		var it_autoload := get_node(^"/root/InputTracker")
@@ -161,7 +189,19 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 ## Used by the Plugin to skip fields.
 func should_skip_property(field: String) -> bool:
-	return field == "actions"
+	if field == "actions":
+		return true
+	
+	if field.begins_with("timeout_") and not clear_on_timeout:
+		return true
+
+	if field == "clear_on_release_only_when_all_are_released":
+		return not (clear_on_action_release or (clear_on_timeout and timeout_clear_on_action_release))
+
+	if field == "trigger_echo_counts_as_press" and not trigger_consider_only_if_current:
+		return true
+	
+	return false
 
 #endregion
 
@@ -182,8 +222,15 @@ func is_valid(cache: Dictionary = {}) -> bool:
 
 ## When we exit the mood this condition belongs to, we always set ourselves to
 ## invalid.
+func _enter_mood(_next_mood: Mood) -> void:
+	if clear_on_mood_enter:
+		_valid = invert_validity
+
+## When we exit the mood this condition belongs to, we always set ourselves to
+## invalid.
 func _exit_mood(_next_mood: Mood) -> void:
-	_valid = false
+	if clear_on_mood_exit:
+		_valid = invert_validity
 
 #endregion
 
@@ -196,40 +243,57 @@ func _on_action_pressed(action: String, event: InputEvent) -> void:
 	if action not in actions:
 		return
 
+	if trigger_consider_only_if_current and not mood.is_current_mood():
+		return
+
 	if action not in _pressed_actions:
 		_pressed_actions.append(action)
 
-	if !rule_all_actions or _pressed_actions.size() == actions.size():
+	if !trigger_require_all_actions or _pressed_actions.size() == actions.size():
 		_valid = !invert_validity
-		if rule_become_invalid_when in [InvalidTrigger.TIMEOUT, InvalidTrigger.TIMEOUT_OR_RELEASE]:
-			if _timer == null or rule_timeout_refresh_on_echo:
+
+		if clear_on_timeout:
+			if _timer == null or timeout_reset_on_action_echo:
 				if _timer != null: # refreshing by killing
 					_timer.timeout.disconnect(_on_timeout)
-				_timer = get_tree().create_timer(rule_timeout_sec, false)
+				_timer = get_tree().create_timer(timeout_timeout_seconds, false)
 				_timer.timeout.connect(_on_timeout)
 
 func _on_action_echoed(action: String, event: InputEvent) -> void:
 	if action not in actions:
 		return
 
-	if _timer != null and rule_timeout_refresh_on_echo:
+	if trigger_consider_only_if_current:
+		if not mood.is_current_mood():
+			return
+
+		if action not in _pressed_actions and trigger_echo_counts_as_press:
+			_on_action_pressed(action, event) # treat this like initial press first.
+
+	if action in _pressed_actions and _timer != null and timeout_reset_on_action_echo:
 		_timer.timeout.disconnect(_on_timeout)
-		_timer = get_tree().create_timer(rule_timeout_sec, false)
+		_timer = get_tree().create_timer(timeout_timeout_seconds, false)
 
 func _on_action_released(action: String, event: InputEvent) -> void:
 	if action not in actions:
 		return
 
+	if action not in _pressed_actions:
+		return
+
+	if trigger_consider_only_if_current and not mood.is_current_mood():
+		return
+
 	_pressed_actions.erase(action)
 
-	if _timer != null and rule_reset_timer_on_release: # refreshing by killing
-		_timer.timeout.disconnect(_on_timeout)
-		_timer = null
-
-	if rule_become_invalid_when in [InvalidTrigger.RELEASE, InvalidTrigger.TIMEOUT_OR_RELEASE]:
-		if !rule_only_invalid_when_all_released or _pressed_actions.is_empty():
+	if clear_on_action_release:
+		if (not clear_on_release_only_when_all_are_released) or _pressed_actions.is_empty():
 			_valid = invert_validity
+
+	if _timer != null and timeout_clear_on_action_release: # refreshing by killing
+		if (not clear_on_release_only_when_all_are_released) or _pressed_actions.is_empty():
+			_timer.timeout.disconnect(_on_timeout)
+			_timer = null
 
 func _on_timeout() -> void:
 	_valid = invert_validity
-	_timer = null
